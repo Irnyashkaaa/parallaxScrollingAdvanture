459ad0b77b4856292cf28105294275b7f78979cc
Iryna <irynafurhala@gmail.com>
2022-10-11 17:11:34 +0300

    add space-page

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const width = window.innerWidth
+const height = window.innerHeight
+
 let text = document.getElementById('text')
 let bird1 = document.getElementById('bird1')
 let bird2 = document.getElementById('bird2')
@@ -5,15 +8,32 @@ let forest = document.getElementById('forest')
 let rocks = document.getElementById('rocks')
 let water = document.getElementById('water')
 
-
-window.addEventListener('scroll', function() {
+let earth = document.getElementById('planet_earth')
+let planet_mid = document.getElementById('planet_mid')
+let planet_foreground = document.getElementById('planet_foreground')
+window.addEventListener('scroll', function () {
     let value = window.scrollY;
-    
-    text.style.top = 50 + value * -0.3 + '%'
-    bird1.style.top = value * -1.5 + 'px'
-    bird1.style.left = value * 2 + 'px'
-    bird2.style.top = value * -0.5 + 'px'
-    bird2.style.left = value * -3.5 + 'px'
 
-    forest.style.top = value * 0.1 + 'px'
+    if (bird1) {
+
+        text.style.top = 50 + value * -0.3 + '%'
+        bird1.style.top = value * -1.5 + 'px'
+        bird1.style.left = value * 2 + 'px'
+        bird2.style.top = value * -0.5 + 'px'
+        bird2.style.left = value * -3.5 + 'px'
+
+        forest.style.top = value * 0.1 + 'px'
+    } 
+
+})
+
+
+window.addEventListener('mousemove', (e) => {
+    if (earth) {
+        earth.style.top = e.y * 0.06 + 'px'
+        earth.style.left = e.x * 0.03 + width / 2 - earth.naturalWidth / 10 + 'px'
+        planet_mid.style.top = e.y * 0.01 + 'px'
+        planet_foreground.style.top = e.y * 0.005 + 'px'
+    }
+
 })
